258710c20a56aa078a081590e85159ca240b30af
Zahidul Islam  Sagor <117823487+sagor2315@users.noreply.github.com>
2025-04-13 16:09:50 +0600

    max with updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,1 @@
-module.exports = {
-  content: ["./src/**/*.{html,js}"], // আপনার ফাইল পাথ অনুযায়ী এটি সংশোধন করুন
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-       
+// tailwind.config.js
